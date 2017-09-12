348af44a925b1240b2b283d7716d855a9530f74b
Billy <billychartanto@gmail.com>
2017-09-12 10:02:05 +0800

    initial commit

---
+++ springpbi/src/main/java/com/allnewthor/springpbi/SpringpbiApplication.java
@@ -0,0 +1,12 @@
+package com.allnewthor.springpbi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringpbiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringpbiApplication.class, args);
+	}
+}
